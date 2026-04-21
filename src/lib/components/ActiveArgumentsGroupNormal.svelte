d4aa797426b18145cedb224e4768649b8e652b41
Marcel <development.vargamarcel@gmail.com>
2026-04-21 10:48:29 +0200

    fixed input fields bugs

--- src/lib/components/ActiveArgumentsGroupNormal.svelte
+++ src/lib/components/ActiveArgumentsGroupNormal.svelte
@@ -91,7 +91,7 @@
 	onfinalize={handleFinalize}
 	class=" rounded-box pt-2 pr-2"
 >
-	{#each group.group_args as any[] as activeArgumentData (activeArgumentData.id)}
+	{#each group.group_args as _, i (group.group_args[i].id)}
 		<div animate:flip={{ duration: flipDurationMs }} class="relative flex">
 			{#if $dndIsOn}
 				<div class="grid content-center rounded-full">
@@ -128,7 +128,7 @@
 					onInUseChanged={() => {
 						onUpdateQuery?.();
 					}}
-					{activeArgumentData}
+					bind:activeArgumentData={group.group_args[i]}
 					{group}
 					{activeArgumentsDataGrouped}
 				/>
